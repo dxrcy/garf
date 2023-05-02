3b2b1af79f2348f9cd946493ffe2d0ea991d7912
darcy <darcy3057@gmail.com>
2023-05-02 18:24:04 +1000

    Made API good

---
+++ src/lib.rs
@@ -0,0 +1,88 @@
+use chrono::{Datelike, NaiveDate};
+use thiserror::Error;
+
+/// Error fetching image URL for comic
+#[derive(Debug, Error)]
+pub enum ComicError {
+    #[error("Request failed")]
+    Request(reqwest::Error),
+    #[error("Image link was not found in webpage body")]
+    ImageLinkNotFound,
+    #[error("Image link was not formatted in ASCII text")]
+    InvalidImageLinkEncoding,
+}
+
+/// Get image URL of comic, asynchronously, given a date (`NaiveDate`)
+pub async fn comic_url(date: NaiveDate) -> Result<String, ComicError> {
+    // Convert date to YYYY/MM/DD string
+    let date_string = date_to_string(date);
+
+    // Get webpage url from date string
+    let url = format!(
+        "https://corsproxy.garfieldapp.workers.dev/cors-proxy?https://www.gocomics.com/garfield/{}",
+        date_string
+    );
+
+    // Fetch webpage body
+    let response_body = fetch_body(&url).await.map_err(ComicError::Request)?;
+
+    // Find image url in HTML
+    let Some(char_index) = response_body.find("https://assets.amuniversal.com") else {
+        return Err(ComicError::ImageLinkNotFound);
+    };
+
+    // Get string from character index
+    let Some(image_url) = response_body.get(char_index..char_index + 63) else {
+        return Err(ComicError::InvalidImageLinkEncoding);
+    };
+
+    Ok(image_url.to_string())
+}
+
+/// Fetch HTML body from URL
+async fn fetch_body(url: &str) -> Result<String, reqwest::Error> {
+    reqwest::Client::new().get(url).send().await?.text().await
+}
+
+/// Convert `NaiveDate` to YYYY/MM/DD format
+fn date_to_string(date: NaiveDate) -> String {
+    date.year().to_string() + "/" + &date.month().to_string() + "/" + &date.day().to_string()
+}
+
+/// Get relevant dates, as `NaiveDate`
+pub mod date {
+    use chrono::{NaiveDate, NaiveDateTime, NaiveTime, Utc};
+    use rand::Rng;
+
+    /// Today's date, as `NaiveDate`
+    pub fn today() -> NaiveDate {
+        Utc::now().date_naive()
+    }
+
+    /// Date of first comic, as `NaiveDate`
+    pub fn first() -> NaiveDate {
+        NaiveDate::from_ymd_opt(1978, 6, 19).expect("Static date failed to parse")
+    }
+
+    /// Random date, between date of first comic `date::first()`, and today's date
+    /// (`date::today`)
+    pub fn random() -> NaiveDate {
+        // Get timestamp of first date
+        let first_date = first().and_time(NaiveTime::MIN).timestamp();
+
+        // Get timestamp of today's date
+        let today_date = Utc::now().timestamp();
+
+        // Convert to range
+        let range = first_date..=today_date;
+
+        // Get random timestamp in range
+        let timestamp = rand::thread_rng().gen_range(range);
+
+        // Convert timestamp to datetime
+        let datetime = NaiveDateTime::from_timestamp_opt(timestamp, 0).unwrap();
+
+        // Convert to date
+        datetime.date()
+    }
+}
